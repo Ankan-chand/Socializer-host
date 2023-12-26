d3e55e536dd78bd3fb9fc51736827045825ab3a5
Ankan chand <chandankan6294@gmail.com>
2023-12-26 17:38:07 +0530

    adding domain

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -94,7 +94,8 @@ exports.userLogin = catchAsyncError(async (req, res, next) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    // sameSite: 'none',
+    sameSite: 'none',
+    domain: 'https://socializer-39eg.onrender.com',
     secure: true
   };
 
